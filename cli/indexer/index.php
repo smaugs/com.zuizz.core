693beaf4f0a220ae0b7d1f0fc66f0b82ad23e0ad
veith <veithz@gmail.com>
2013-01-25 09:48:10 +0100

    Itemlist

--- cli/indexer/index.php
+++ cli/indexer/index.php
@@ -1,8 +1,19 @@
 <?php
 
-ZU::load_class('stemmer.de','core/search');
+
+ZU::load_class('index','core/search');
 
 
-$stemmer = new PorterStemmerDE;
+$index = new ZUINDEX();
 
-echo $stemmer->Stem($this->values['word']);
+$index->index('Warum ich das hinzufügen von Adressen aus den Erfassungsmasken (Case, Aktivität,..) entfernt habe:
+
+Ein Case oder eine Aktivität entsteht nicht einfach so. Bevor ein Sales MA etwas bei einem Account unternimmt, vergewissert er sich im Custory ob schon etwas bei diesem Account gemacht wurde oder was gerade bei diesem läuft.
+
+Existiert der Account nicht, wird der Sales MA diesen erfassen und erst dann eine Aktivität oder einen Case haben.
+
+Allein dieser Schritt kann die Datenqualität verbessern und eine etwas geordnetere Arbeitsweise forcieren.
+
+Sollte ich mit meiner Einschätzung falsch liegen, teilt mir dies bitte mit.
+'
+);
