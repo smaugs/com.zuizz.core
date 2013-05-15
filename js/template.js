c29ec98145d12a373b460a5585c0eaee8447f33d
Veith Zäch <veithz@gmail.com>
2013-05-15 15:53:50 +0200

    Tc Template

--- js/template.js
+++ js/template.js
@@ -16,60 +16,22 @@
          * @param {Sandbox} sandbox the sandbox to get the resources from
          * @param {String} modId the unique module id
          */
-        init:function ($ctx, sandbox, modId) {
+        init: function ($ctx, sandbox, modId) {
             var self = this;
             // call base constructor
             this._super($ctx, sandbox, modId);
 
             self.initDot();
+            this.registerListener(sandbox);
         },
-
-
-
-
-        clearContent:function(){
-            var $ctx = this.$ctx,
-                self = this;
-
-        },
-        loadData: function(){
-            var $ctx = this.$ctx,
-                self = this;
-
-            $.ajax({
-                url:'/rest/***.******.****.templates/.json',
-                type:'GET',
-                data:{
-                    id: self.id   //int
-                },
-
-                statusCode:{
-                    200:function(response){
-
-                        self.renderData(response.data);
-
-                    },
-                    204:function(response){
-                        //Es sind keine Daten für die gestellte Anfrage vorhanden.
-                    }
-                }
-            });
-        },
-        renderData: function(data){
-            var $ctx = this.$ctx,
-                self = this;
-            $('.textblock').html(self.dot.textblock(data))
-            $('.textblock').fadeIn();
-        },
-
-         /**
+        /**
          * Hook function to do all of your module stuff.
          *
          * @method on
          * @param {Function} callback function
          * @return void
          */
-        on:function (callback) {
+        on: function (callback) {
             var $ctx = this.$ctx,
                 self = this;
             callback();
@@ -81,10 +43,53 @@
          * @method after
          * @return void
          */
-        after:function () {
+        after: function () {
 
-        } ,
-        initDot:function () {
+        },
+
+        registerListener: function (sandbox) {
+            var $ctx = this.$ctx,
+                self = this;
+
+        },
+
+        clearContent: function () {
+            var $ctx = this.$ctx,
+                self = this;
+
+        },
+        loadData: function () {
+            var $ctx = this.$ctx,
+                self = this;
+
+            $.ajax({
+                url: '/rest/***.******.****.templates/.json',
+                type: 'GET',
+                data: {
+                    id: self.id   //int
+                },
+
+                statusCode: {
+                    200: function (response) {
+
+                        self.renderData(response.data);
+
+                    },
+                    204: function (response) {
+                        //Es sind keine Daten für die gestellte Anfrage vorhanden.
+                    }
+                }
+            });
+        },
+        renderData: function (data) {
+            var $ctx = this.$ctx,
+                self = this;
+            $('.textblock').html(self.dot.textblock(data))
+            $('.textblock').fadeIn();
+        },
+
+
+        initDot: function () {
             var $ctx = this.$ctx,
                 self = this;
             self.dot = {};
